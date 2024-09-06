f09f286165bc9d2aa873659a06fccd4bd1a5c7c6
Victa <corgi.desu@gmail.com>
2024-09-07 09:20:57 +1000

    update: typescript config to remove blockchain

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules?.push({
-      test: 'blockchain',
-      loader: "ignore-loader",
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
