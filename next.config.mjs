e0e0f60474b44e46f6f45f63f25d36a2238b996b
Victa <corgi.desu@gmail.com>
2024-09-07 09:12:19 +1000

    update: ignore blockchain sub-folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules?.push({
+      blockchain: "blockchain",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
